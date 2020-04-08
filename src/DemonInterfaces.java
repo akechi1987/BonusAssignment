abc473e32826e7722af8c1650b51f1d37751a34a
Akechi <Fabregas87>
2020-04-08 13:55:08 -0500

    Initial commit

---
+++ src/DemonInterfaces.java
@@ -0,0 +1,2 @@
+public class DemonInterfaces {
+}
---
+++ src/ItemShop.java
@@ -0,0 +1,9 @@
+public class ItemShop {
+    int ItemCode;
+    String ItemName;
+    ItemShop(int ICode, String IName){
+        ItemCode=ICode;
+        ItemName=IName;
+    }
+}
+
